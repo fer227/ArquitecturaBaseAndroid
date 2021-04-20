772a5c3a0bb46d8d844e5fc81c9b4f0854311ece
Fernando Izquierdo Romera <227fer@gmail.com>
2021-04-20 18:04:03 +0200

    Petición en Retrofit completada, aún por testear

--- app/src/main/java/com/app/server/Formulario.java
+++ app/src/main/java/com/app/server/Formulario.java
@@ -10,6 +10,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.app.server.api.RetrofitAPI;
+import com.app.server.models.Modelo;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,12 +25,13 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Formulario extends AppCompatActivity {
-
-    String url = "";
+    private String url = "";
     //Debe terminar en /
     protected void enviarToast(String msg){
         Context context = getApplicationContext();
@@ -59,10 +62,10 @@ public class Formulario extends AppCompatActivity {
             public void onClick(View v) {
                 JSONObject parametros = new JSONObject();
                 try {
-                    parametros.put("artista", artista);
-                    parametros.put("cancion", cancion);
-                    parametros.put("duracion", duracion);
-                    parametros.put("productores", productores);
+                    parametros.put("artista", artista.getText().toString());
+                    parametros.put("cancion", cancion.getText().toString());
+                    parametros.put("duracion", duracion.getText().toString());
+                    parametros.put("productores", productores.getText().toString());
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -73,7 +76,16 @@ public class Formulario extends AppCompatActivity {
                             @Override
                             public void onResponse(JSONObject response) {
                                 try {
-                                    enviarToast(response.getString("msg"));
+                                    if(response.getString("result").equals("200")){
+                                        try {
+                                            enviarToast(response.getString("msg"));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                    else{
+                                        enviarToast("No se pudo mandar la petición");
+                                    }
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
@@ -96,10 +108,26 @@ public class Formulario extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
+        RetrofitAPI servicioAPI = retrofit.create(RetrofitAPI.class);
+
         enviarRetrofit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                servicioAPI.postCancion(artista.getText().toString(), cancion.getText().toString(), duracion.getText().toString(), productores.getText().toString()).enqueue(
+                        new Callback<Modelo>() {
+                            @Override
+                            public void onResponse(Call<Modelo> call, retrofit2.Response<Modelo> response) {
+                                if(response.code() == 200){
+                                    enviarToast(response.message());
+                                }
+                            }
 
+                            @Override
+                            public void onFailure(Call<Modelo> call, Throwable t) {
+                                enviarToast("No se pudo mandar la petición");
+                            }
+                        }
+                );
             }
         });
     }
