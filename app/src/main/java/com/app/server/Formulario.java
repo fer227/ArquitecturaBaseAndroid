66b538621b2c8713b75753f70b29f8e57de87707
Fernando Izquierdo Romera <227fer@gmail.com>
2021-04-20 16:38:53 +0200

    Añadida petición de Volley. También una Toast que indica si se la solicitud ha tenido éxito

--- app/src/main/java/com/app/server/Formulario.java
+++ app/src/main/java/com/app/server/Formulario.java
@@ -1,6 +1,14 @@
 package com.app.server;
 
+import android.content.Context;
 import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +17,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Formulario extends AppCompatActivity {
+
+    String url = "";
+    protected void enviarToast(String msg){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = null;
+        toast = Toast.makeText(context, msg, duration);
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +48,40 @@ public class Formulario extends AppCompatActivity {
         EditText duracion   = (EditText) findViewById(R.id.text_duracion);
         EditText productores   = (EditText) findViewById(R.id.text_productores);
 
+        RequestQueue queueVolley = Volley.newRequestQueue(this);
         enviarVolley.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                JSONObject parametros = new JSONObject();
+                try {
+                    parametros.put("artista", artista);
+                    parametros.put("cancion", cancion);
+                    parametros.put("duracion", duracion);
+                    parametros.put("productores", productores);
 
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, parametros,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    enviarToast(response.getString("msg"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                enviarToast("No se pudo mandar la petición");
+                            }
+                        }
+                );
+                queueVolley.add(request);
             }
         });
 
